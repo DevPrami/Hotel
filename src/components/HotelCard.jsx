a9d3deb8aa4cfa882010fb583d65bbf8f80547da
DevPrami <developerpramila@gmail.com>
2025-07-17 21:18:25 +0530

    Testimonial

--- src/components/HotelCard.jsx
+++ src/components/HotelCard.jsx
@@ -7,13 +7,13 @@ export default function HotelCard({ room, index }) {
     <Link
       to={"/rooms/" + room._id}
       onClick={() => scrollTo(0,0)}
-      Key={room._id}
+      Key={room._id} className="relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0, 0 , 0 , 0.05)]"
+
     >
       <img
         src={room.images[0]}
         alt=""
-        className="relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0, 0 , 0 , 0.05)]"
-      />
+              />
 
       {index % 2 === 0 && (
         <p className="px-3 py-1 absolute top-3 left-3 text-xs bg-white text-gray-800 font-medium rounded-full">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import FeatureDestination from '../components/FeatureDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
 
 export default function () {
   return (
     <>
         <Hero/>
         <FeatureDestination/>
+        <ExclusiveOffers/>
+        <Testimonial/>
+        
     </>
   )
 }
