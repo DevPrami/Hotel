dd0a42e32a55a7d83254363da3a7b99501f72767
DevPrami <developerpramila@gmail.com>
2025-07-20 20:53:48 +0530

    room

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<AllRooms />} />
         
       </Routes>
       <Footer/>
