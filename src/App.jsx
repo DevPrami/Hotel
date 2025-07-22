0c9db91b054571356fcdba16d962b3b64dc7d281
DevPrami <developerpramila@gmail.com>
2025-07-22 17:43:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<AllRooms />} />
+        <Route path="/rooms/" element={<AllRooms />} />
         
       </Routes>
       <Footer/>
